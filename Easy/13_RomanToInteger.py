d25ed0773067e924e8512fe4550420f68771a015
ZhangYixin0618 <YixinZhang0618@gmail.com>
2020-05-03 19:41:26 +0100

    Upload other things

--- Easy/13_RomanToInteger.py
+++ Easy/13_RomanToInteger.py
@@ -1,22 +1,10 @@
 def romanToInt(s):
     sum = 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for index in range(0, len(s)):
-        try:
-            if s[index:(index + 2)] == "IV":
-                sum = sum + 4 - 5
-            elif s[index:(index + 2)] == "IX":
-                sum = sum + 9 - 10
-            elif s[index:(index + 2)] == "XL":
-                sum = sum + 40 - 50
-            elif s[index:(index + 2)] == "XC":
-                sum = sum + 90 - 100
-            elif s[index:(index + 2)] == "CD":
-                sum = sum + 400 - 500
-            elif s[index:(index + 2)] == "CM":
-                sum = sum + 900 - 1000
-            else:
-                sum = sum + roman_dict[s[index]]
-        except IndexError:
-            sum = sum + roman_dict[s[index]]
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'A': 4, 'B': 9, 'E': 40, 'F': 90,
+                  'G': 400, 'H': 900}
+    s = s.replace("IV", "A").replace("IX", "B").replace("XL", "E").replace("XC", "F").replace("CD", "G").replace("CM",
+                                                                                                                 "H")
+    for item in s:
+        sum += roman_dict[item]
     return sum
+print(romanToInt('IV'))
