407056c04c76ac8bac7564086e5163cbf0540660
ZhangYixin0618 <64661357+ZhangYixin0618@users.noreply.github.com>
2020-05-05 13:08:37 +0100

    Add files via upload

---
+++ Easy/MergeTwoSortedLists21.java
@@ -0,0 +1,47 @@
+class MergeTwoSortedLists21 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode l3 = dummy;
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                l3.next = l1;
+                l1 = l1.next;
+            } else {
+                l3.next = l2;
+                l2 = l2.next;
+            }
+            l3 = l3.next;
+        }
+        if (l1 == null) {
+            l3.next = l2;
+        }
+        if (l2 == null) {
+            l3.next = l1;
+        }
+        return dummy.next;
+    }
+
+}
